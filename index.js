2f2951c262109c543be8477fba63ac518116d786
jianglibo <jianglibo@gmail.com>
2016-01-22 15:52:52 +0800

    return parse true in process

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 var DtreeConverter = require("my-ascii-tree");
 
 module.exports = {
-    blocks: {
-        asciitree: {
-            process: function(block) {
-              var body = block.body || "";
-              var lines = body.split("\n");
-              console.log(this); // this will printed in gitbook console.
-              var convertedLines = new DtreeConverter(lines).convert() || [];
-              // if surrounded by ```, block will not be processed.
-              convertedLines.unshift("```");
-              convertedLines.push("```");
-              return convertedLines.join("\n");
-            }
-        }
+  blocks: {
+    asciitree: {
+      // shortcuts: {
+      //   parsers: ["markdown"],
+      //   start: "$$",
+      //   end: "$$"
+      // },
+      process: function(block) {
+        var body = block.body || "";
+        var lines = body.split("\n");
+        console.log(this); // this will printed in gitbook console.
+        var convertedLines = new DtreeConverter(lines).convert() || [];
+        // if surrounded by ```, block will not be processed.
+        convertedLines.unshift("```");
+        convertedLines.push("```");
+        return {
+          body: convertedLines.join("\n"),
+          parse: true
+        };
+      }
     }
+  }
 };
+
+/*
+process can return this formatter too.
+return {
+    body: lang+"_"+blk.body+"_"+lang,
+    html: false
+};
+https://github.com/GitbookIO/gitbook/blob/master/lib/plugin.js
+// Get config from book
+Plugin.prototype.getConfig = function() {
+    return this.book.config.get('pluginsConfig.'+this.reducedName(), {});
+};
+*/
